731b1687a326c2345ac7ef5a6df98ba63b0e54af
Joel Truher <joel@truher.org>
2026-04-21 16:14:08 -0700

    deploy the systemcore jni

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,5 +1,7 @@
 package frc.robot;
 
+import java.io.File;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 
 public class Robot extends TimedRobot {
@@ -7,17 +9,37 @@ public class Robot extends TimedRobot {
     private final Sim sim;
 
     public Robot() {
+        System.out.println("Robot()");
+        String cwd = new File("").getAbsolutePath();
+        System.out.println("*************************************************************************************");
+        System.out.printf("CWD %s\n", cwd);
+        System.out.println("*************************************************************************************");
         sim = Sim.make();
+        // run once
+        try {
+            sim.run();
+        } catch (Throwable e) {
+            System.out.println("*************************************************************************************");
+            e.printStackTrace();
+        }
+        // sim=null;
     }
 
     @Override
     public void teleopPeriodic() {
+        System.out.println("*************************************************************************************");
+        System.out.println("teleopPeriodic()");
         if (sim != null) {
             try {
                 sim.run();
             } catch (Throwable e) {
+                System.out.println("*************************************************************************************");
                 e.printStackTrace();
             }
+        } else {
+            System.out.println("*************************************************************************************");
+            System.out.println("sim is null");
         }
+        System.out.flush();
     }
 }
